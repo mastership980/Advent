f148b53dd0912d6bc845e56f92fcfc29af099f07
Rácz Norbert <racz.norbert@labnet.tk>
2023-11-15 11:25:32 +0100

    feles commentek kiszedve

--- main.js
+++ main.js
@@ -72,360 +72,360 @@ function sClick(){ // 19.
 }
 
 document.addEventListener("DOMContentLoaded", function aClick() { //1
-  var specialDate = new Date('2023-12-01'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-01'); 
   var today = new Date();
 
   var specialButton = document.getElementById('a');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function qClick() { //17
-  var specialDate = new Date('2023-12-17'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-17'); 
   var today = new Date();
 
   var specialButton = document.getElementById('q');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //20
-  var specialDate = new Date('2023-12-20'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-20'); 
   var today = new Date();
 
   var specialButton = document.getElementById('t');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //9
-  var specialDate = new Date('2023-12-09'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-09'); 
   var today = new Date();
 
   var specialButton = document.getElementById('i');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //5
-  var specialDate = new Date('2023-12-05'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-05'); 
   var today = new Date();
 
   var specialButton = document.getElementById('e');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //2
-  var specialDate = new Date('2023-12-02'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-02'); 
   var today = new Date();
 
   var specialButton = document.getElementById('b');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //14
-  var specialDate = new Date('2023-12-14'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-14'); 
   var today = new Date();
 
   var specialButton = document.getElementById('n');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //4
-  var specialDate = new Date('2023-12-04'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-04'); 
   var today = new Date();
 
   var specialButton = document.getElementById('d');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //8
-  var specialDate = new Date('2023-12-08'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-08'); 
   var today = new Date();
 
   var specialButton = document.getElementById('h');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //6
-  var specialDate = new Date('2023-12-06'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-06'); 
   var today = new Date();
 
   var specialButton = document.getElementById('f');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //3
-  var specialDate = new Date('2023-12-03'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-03'); 
   var today = new Date();
 
   var specialButton = document.getElementById('c');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //24
-  var specialDate = new Date('2023-12-24'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-24'); 
   var today = new Date();
 
   var specialButton = document.getElementById('x');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //11
-  var specialDate = new Date('2023-12-11'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-11'); 
   var today = new Date();
 
   var specialButton = document.getElementById('k');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //10
-  var specialDate = new Date('2023-12-10'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-10'); 
   var today = new Date();
 
   var specialButton = document.getElementById('j');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //15
-  var specialDate = new Date('2023-12-15'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-15'); 
   var today = new Date();
 
   var specialButton = document.getElementById('o');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //7
-  var specialDate = new Date('2023-12-07'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-07'); 
   var today = new Date();
 
   var specialButton = document.getElementById('g');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //13
-  var specialDate = new Date('2023-12-13'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-13'); 
   var today = new Date();
 
   var specialButton = document.getElementById('m');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //12
-  var specialDate = new Date('2023-12-12'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-12'); 
   var today = new Date();
 
   var specialButton = document.getElementById('l');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //16
-  var specialDate = new Date('2023-12-16'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-16'); 
   var today = new Date();
 
   var specialButton = document.getElementById('p');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //21
-  var specialDate = new Date('2023-12-21'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-21'); 
   var today = new Date();
 
   var specialButton = document.getElementById('u');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //22
-  var specialDate = new Date('2023-12-22'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-22'); 
   var today = new Date();
 
   var specialButton = document.getElementById('v');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //23
-  var specialDate = new Date('2023-12-23'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-23'); 
   var today = new Date();
 
   var specialButton = document.getElementById('w');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //18
-  var specialDate = new Date('2023-12-18'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-18'); 
   var today = new Date();
 
   var specialButton = document.getElementById('r');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
 });
 
 document.addEventListener("DOMContentLoaded", function aClick() { //19
-  var specialDate = new Date('2023-12-19'); // Set your special date here in 'YYYY-MM-DD' format
+  var specialDate = new Date('2023-12-19'); 
   var today = new Date();
 
   var specialButton = document.getElementById('s');
 
   if (today.toDateString() === specialDate.toDateString()) {
-    // Button is clickable, no need to add the class or remove the disabled attribute
+    
   } else {
-    // Button is not clickable, remove the clickable class and add the disabled attribute
+    
     specialButton.classList.remove('clickable');
     specialButton.setAttribute('disabled', 'disabled');
   }
