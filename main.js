18f333a84b1186b9d84e24482d28486cc7845c3c
Mastership980 <r.lacus25@gmail.com>
2023-11-20 19:55:46 +0100

    Igen

--- main.js
+++ main.js
@@ -6,16 +6,23 @@ const currentDay = today.getDate()
 
 
 items.forEach(item => {
-  if(currentDay == item.textContent.trim())
+  if(item.firstElementChild.textContent.trim() < currentDay)
+  {
+    item.classList.add("done")
+  }
+  if(currentDay == item.firstElementChild.textContent.trim())
   {
     item.classList.add("current")
   }
-  
   item.onclick = () => {
+    //Ha a mai nap és a div ben lévő szám eggyezik, 
+    //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
+    //akkor fusson le
     item.style.zIndex = "1000"
     item.classList.add("opened")
-    if(currentDay == item.textContent.trim())
+    if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "11")
     {
+      //zIndex azért hogy amikor kinyílik ne legyen overlap
     }
     else
     {
