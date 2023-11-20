34193cbcb3a43ce82ba33c236490ee1d47e6cc74
Mastership980 <r.lacus25@gmail.com>
2023-11-21 00:36:04 +0100

    Igen

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
 const items = document.querySelectorAll(".item")
 
-const today = new Date()
+const a = Date.now()
+
+const today = new Date(a)
 const currentMonth = today.getMonth()
 const currentDay = today.getDate()
 
+let index = 0
 
-items.forEach(item => {
-  if(item.firstElementChild.textContent.trim() < currentDay)
+const interval = setInterval(() => {
+  if(index < items.length)
   {
-    item.classList.add("done")
-  }
-  if(currentDay == item.firstElementChild.textContent.trim())
-  {
-    item.classList.add("current")
-  }
-  item.onclick = () => {
-    //Ha a mai nap és a div ben lévő szám eggyezik, 
-    //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
-    //akkor fusson le
-    item.style.zIndex = "1000"
-    item.classList.add("opened")
-    if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "11")
+    const item = items[index]
+
+    if(item.firstElementChild.textContent.trim() < currentDay)
     {
-      //zIndex azért hogy amikor kinyílik ne legyen overlap
+      item.classList.add("done")
     }
-    else
+
+    if(currentDay == item.firstElementChild.textContent.trim())
     {
-      return
+      item.classList.add("current")
     }
+
+    item.onclick = () => {
+      //Ha a mai nap és a div ben lévő szám eggyezik, 
+      //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
+      //akkor fusson le
+      if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "10")
+      {
+        //zIndex azért hogy amikor kinyílik ne legyen overlap
+        item.style.zIndex = "1000"
+        item.classList.add("opened")
+      }
+      else
+      {
+        return
+      }
+    }
+
+    index++
   }
-})
+  else{
+    clearInterval(interval)
+  }
+}, 50)
