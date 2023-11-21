0727d7f6eed219348d0c2651528dd9677a8fe8ca
Mastership980 <r.lacus25@gmail.com>
2023-11-21 11:02:09 +0100

    Logika kész, hátra vannak az üzenetek

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const items = document.querySelectorAll(".item")
 
-const a = Date.now()
-
-const today = new Date(a)
+const today = new Date()
 const currentMonth = today.getMonth()
 const currentDay = today.getDate()
 
 let index = 0
+
+if(localStorage.getItem("isDone") === null || 
+   localStorage.getItem("savedDay") === null)
+{
+  localStorage.setItem("isDone", false.toString())
+  localStorage.setItem("savedDay", "0")
+}
 
 const interval = setInterval(() => {
   if(index < items.length)
   {
     const item = items[index]
 
-    if(item.firstElementChild.textContent.trim() < currentDay)
+    if(item.firstElementChild.textContent.trim() == currentDay && 
+       localStorage.getItem("isDone") === "true" &&
+       localStorage.getItem("savedDay") == currentDay)
     {
       item.classList.add("done")
     }
-
+    else if(item.firstElementChild.textContent.trim() < currentDay && currentMonth == "11")
+    {
+      item.classList.add("done")
+    }
     if(currentDay == item.firstElementChild.textContent.trim())
     {
       item.classList.add("current")
@@ -27,11 +37,13 @@ const interval = setInterval(() => {
       //Ha a mai nap és a div ben lévő szám eggyezik, 
       //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
       //akkor fusson le
-      if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "10")
+      if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "11")
       {
         //zIndex azért hogy amikor kinyílik ne legyen overlap
         item.style.zIndex = "1000"
         item.classList.add("opened")
+        localStorage.setItem("isDone", true.toString())
+        localStorage.setItem("savedDay", currentDay.toString())
       }
       else
       {
