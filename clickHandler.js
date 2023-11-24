0f3c990df8a9f81d29ea657e962a222c3b9c7642
Mastership980 <r.lacus25@gmail.com>
2023-11-24 10:39:41 +0100

    még több dolog hozzáadva meeeeeauuuuuwwww

--- clickHandler.js
+++ clickHandler.js
@@ -4,6 +4,7 @@ const today = new Date()
 const currentMonth = today.getMonth()
 const currentDay = today.getDate()
 
+const month = 11
 let index = 0
 
 if(localStorage.getItem("isDone") === null || 
@@ -19,16 +20,17 @@ const interval = setInterval(() => {
     const item = items[index]
 
     if(item.firstElementChild.textContent.trim() == currentDay && 
+       currentMonth == month &&
        localStorage.getItem("isDone") === "true" &&
        localStorage.getItem("savedDay") == currentDay)
     {
       item.classList.add("done")
     }
-    else if(item.firstElementChild.textContent.trim() < currentDay && currentMonth == "11")
+    else if(item.firstElementChild.textContent.trim() < currentDay && currentMonth == month)
     {
       item.classList.add("done")
     }
-    if(currentDay == item.firstElementChild.textContent.trim())
+    if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == month)
     {
       item.classList.add("current")
     }
@@ -37,7 +39,7 @@ const interval = setInterval(() => {
       //Ha a mai nap és a div ben lévő szám eggyezik, 
       //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
       //akkor fusson le
-      if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == "11")
+      if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == month)
       {
         //zIndex azért hogy amikor kinyílik ne legyen overlap
         item.style.zIndex = "1000"
