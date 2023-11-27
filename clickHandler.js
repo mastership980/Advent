4e2a456ab38df89bf3b551bac7de7cfb732bf083
Mastership980 <r.lacus25@gmail.com>
2023-11-27 23:16:39 +0100

    day6 ig kész

--- clickHandler.js
+++ clickHandler.js
@@ -1,8 +1,8 @@
 const items = document.querySelectorAll(".item")
 
-const y = Date.now()
+const a = Date.now()
 
-const today = new Date(y)
+const today = new Date(a)
 const currentMonth = today.getMonth()
 const currentDay = today.getDate()
 
@@ -36,11 +36,22 @@ const interval = setInterval(() => {
     {
       item.classList.add("current")
     }
-
+    
+    document.addEventListener("click", (e) => {
+      if(e.target.classList.value.includes("content"))
+      {
+        item.children[1].close()
+        return
+      }
+    })
     item.onclick = () => {
       //Ha a mai nap és a div ben lévő szám eggyezik, 
       //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
       //akkor fusson le
+      if (!item.children[1].open) {
+        item.children[1].showModal()
+        item.children[1].scrollTop = 0
+      }
       if(currentDay == item.firstElementChild.textContent.trim() && currentMonth == month)
       {
         //zIndex azért hogy amikor kinyílik ne legyen overlap
--- modal.js
+++
@@ -1,6 +0,0 @@
-const a = document.querySelector(".item1")
-const modal = document.querySelector(".content")
-
-a.addEventListener("click", () =>{
-    modal.showModal()
-})
