c2b50bafd913ed925f00177dc17e9b6494619664
Mastership980 <r.lacus25@gmail.com>
2023-12-03 17:29:06 +0100

    Responsive fix till day 5

--- clickHandler.js
+++ clickHandler.js
@@ -43,9 +43,6 @@ const interval = setInterval(() => {
       }
     })
     item.onclick = () => {
-      //Ha a mai nap és a div ben lévő szám eggyezik, 
-      //és a hónap az December (JS hónapok azok 0 tól kezdődnek ezért 11 a December)
-      //akkor fusson le
       if (!item.children[1].open) 
       {
         item.children[1].showModal()
